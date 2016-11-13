68db5d750c8c7efcbab5277aaa897e26ccb03983
ivan.zemskiy <ivan.zemskiy@gmail.com>
2016-11-13 14:40:54 +0300

    update test_hooks to cover new 'request' hook

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -9,15 +9,21 @@ def hook(value):
     return value[1:]
 
 
+def hook_tests_generator(*tests):
+    for name in hooks.HOOKS:
+        for test in tests:
+            yield (name,) + test
+
+
 @pytest.mark.parametrize(
-    'hooks_list, result', (
+    'hook_name, hooks_list, result', hook_tests_generator(
         (hook, 'ata'),
         ([hook, lambda x: None, hook], 'ta'),
     )
 )
-def test_hooks(hooks_list, result):
-    assert hooks.dispatch_hook('response', {'response': hooks_list}, 'Data') == result
+def test_hooks(hook_name, hooks_list, result):
+    assert hooks.dispatch_hook(hook_name, {hook_name: hooks_list}, 'Data') == result
 
 
 def test_default_hooks():
-    assert hooks.default_hooks() == {'response': []}
+    assert hooks.default_hooks() == {'response': [], 'request': []}
